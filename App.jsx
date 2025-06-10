e8f56094651d494c9e940499943f8e636058545a
007lucus <007lucus@proton.me>
2025-06-11 01:59:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-white bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Sekkaiichi Frontend Loaded</h1>
+    </div>
+  );
+}
